a8362d49307d8df09ecd3ef2e5b23fdc54f6d6fe
Ghost-IU <projectghost.iu@gmail.com>
2021-05-22 13:16:28 +0530

    Fix database alignment

--- server.py
+++ server.py
@@ -13,12 +13,12 @@ def page_render(page_name):
     return render_template(page_name)
 
 def write_to_csv(data):
-    with open('database.csv', mode='a') as database:
+    with open('database.csv', newline='', mode='a') as database:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         csv_writer = csv.writer(database, delimiter=',', quotechar='"',quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(email,subject,message)
+        csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST','GET'])
 def submit_form():
